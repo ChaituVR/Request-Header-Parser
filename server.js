a796284e9e6d34b80260bc11b44ed34c9b877e21
Chaitanya Raju <yourchaitu@gmail.com>
2016-04-29 11:36:13 +0000

    Done with the project

---
+++ server.js
@@ -0,0 +1,33 @@
+
+var http = require('http');
+var express = require('express');
+var app = express();
+var server = http.createServer(app);
+
+var result = function(ipaddress,browserlanguage,software){
+
+  this.ipaddress=ipaddress;
+  this['language']=browserlanguage;
+  this.software=software;
+};
+
+
+app.get('/api/whoami', function (req, res) {
+  var ip = req.headers['x-forwarded-for'] || 
+     req.connection.remoteAddress;
+
+  var lan=(req.headers["accept-language"]).substr(0,5);
+  var os= req.headers["user-agent"];
+  os=os.substr(os.indexOf("(",0)+1,os.indexOf("\)",0)-os.indexOf("(",0)-1);
+
+  var sendResult=new result(ip,lan,os);
+  res.setHeader('Content-Type', 'application/json');
+  res.send(JSON.stringify(sendResult));
+  
+
+});
+
+server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
+  var addr = server.address();
+  console.log("Server listening at", addr.address + ":" + addr.port);
+});
